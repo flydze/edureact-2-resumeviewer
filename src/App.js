14b050c941e1c62c6be41aff1900e126a3030cd9
Valentyn Zozulia <flydze@gmail.com>
2017-04-16 20:11:06 -0300

    Add json data for the resume. Implement json data import.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
 import './App.css';
 import About from './components/About';
 import Contact from './components/Contact';
@@ -9,7 +10,36 @@ import Resume from './components/Resume';
 import Testimonials from './components/Testimonials';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      foo: 'bar',
+      resumeData: {}
+    };
+  }
+
+  getResumeData() {
+    $.ajax({
+      url: 'http://localhost:3000/resumeData.json',
+      dataType: 'json',
+      cache: false,
+      success: function (data) {
+        this.setState({ resumeData: data });
+      }.bind(this),
+      error: function (xhr, status, err) {
+        console.log(err);
+        alert(err);
+      }
+    });
+  }
+
+  componentDidMount() {
+    this.getResumeData();
+  }
+
   render() {
+    console.log(this.state.resumeData);
+
     return (
       <div className="App">
         <Header />
