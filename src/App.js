20a7872b080b435458df2c1d15e9adda07149e62
Valentyn Zozulia <flydze@gmail.com>
2017-04-16 08:39:03 -0300

    Create the App and Components

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './App.css';
+import About from './components/About';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Portfolio from './components/Portfolio';
+import Resume from './components/Resume';
+import Testimonials from './components/Testimonials';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <About />
+        <Resume />
+        <Portfolio />
+        <Testimonials />
+        <Contact />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
